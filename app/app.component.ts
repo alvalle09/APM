423ee577fd5f77059e18e17d9de7d0f6319c2c4e
Fred Valle <phredvalle@gmail.com>
2017-05-02 17:40:34 -0500

    minor change to test git

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,7 +4,7 @@ import { Component } from '@angular/core';
     selector: 'pm-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
-        <div>My First Component</div>
+        <div>My First Component-Hi!</div>
     </div>    `
 })
 export class AppComponent { 
