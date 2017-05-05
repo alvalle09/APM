02ffd1eb578353e6c568eb424af9c90b6cc8d0ac
alvalle09cls <phredvalle@gmail.com>
2017-05-05 01:32:14 -0500

    Adding Star Component for product ratings

---
+++ app/shared/star.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnChanges } from '@angular/core';
+
+@Component({
+    selector: 'ai-star',
+    templateUrl: 'app/shared/star.component.html',
+    styleUrls: ['app/shared/star.component.css']
+})
+export class StarComponent implements OnChanges{
+    rating: number = 4;
+    starWidth: number;
+
+    ngOnChanges(): void{
+        this.starWidth = this.rating * 86/5;
+    }
+}
