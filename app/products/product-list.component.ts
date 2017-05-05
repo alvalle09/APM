b30806520bf999f230de70cbb034db3d69b3e9c4
alvalle09cls <phredvalle@gmail.com>
2017-05-04 21:22:44 -0500

    Using Product interface, styleUrl in product-list component

--- app/products/product-list.component.ts
+++ app/products/product-list.component.ts
@@ -1,18 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
     selector: 'pm-products',
-    templateUrl: 'app/products/product-list.component.html'
+    templateUrl: 'app/products/product-list.component.html',
+    styleUrls: ['app/products/product-list.component.css']
      
 })
-export class ProductListComponent { 
+export class ProductListComponent implements OnInit { 
     pageTitle: string = 'Product List';
     imageWidth: number = 50;
     imgageMargin: number = 2;
     showImage: boolean = false;
     listFilter: string = 'cart';
     
-    products: any[] = [
+    products: IProduct[] = [
         {
         "productId": 1,
         "productName": "Leaf Rake",
@@ -36,5 +38,9 @@ export class ProductListComponent {
 
     toggleImage(): void{
         this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void{
+        console.log('In product-list OnInit');
     }
 }
