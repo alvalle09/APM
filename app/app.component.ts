eb8ea4268e27149a0c1161be04fe5bd4e9fe5f88
alvalle09cls <phredvalle@gmail.com>
2017-05-09 01:47:02 -0500

    Added ProductService - @Injectable.e

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 
+import { ProductService } from './products/product.service';
+
 @Component({
     selector: 'pm-app',
     template: `
     <div><h1>{{pageTitle}}</h1>
         <pm-products> </pm-products>
-    </div>    `
+    </div>    `,
+    providers: [ProductService]
+
 })
 export class AppComponent { 
     pageTitle: string = 'Acme Product Management';
